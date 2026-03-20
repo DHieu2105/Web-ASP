2155cd0b2d453f2bc542ef811c35f34a67264c1d
DHieu <hyoyoeng2105@gmail.com>
2026-03-21 00:09:56 +0700

    vid8 vs vid9

---
+++ WebApp1/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp1.Models;
+using X.PagedList;
+
+
+namespace WebApp1.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/homeadmin")]
+    public class HomeAdminController : Controller
+    {
+        QlbanVaLiContext db = new QlbanVaLiContext();
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("danhmucsanpham")]
+        public IActionResult DanhMucSanPham(int? page)
+        {
+            int pageSize = 12;
+            int pageNumber = page == null || page < 0 ? 1 : page.Value;
+            var lstsanpham = db.TDanhMucSps.AsNoTracking().OrderBy(x => x.TenSp);
+            PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(lstsanpham, pageNumber, pageSize);
+
+            return View(lst);
+        }
+    }
+}
