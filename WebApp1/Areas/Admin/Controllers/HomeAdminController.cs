f62644d621e3a9f6900b4a56edcfd528d3a397be
DHieu <hyoyoeng2105@gmail.com>
2026-03-21 23:28:58 +0700

    vid10

--- WebApp1/Areas/Admin/Controllers/HomeAdminController.cs
+++ WebApp1/Areas/Admin/Controllers/HomeAdminController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebApp1.Models;
 using X.PagedList;
+using X.PagedList.Mvc.Core; 
 
 
 namespace WebApp1.Areas.Admin.Controllers
@@ -27,6 +29,30 @@ namespace WebApp1.Areas.Admin.Controllers
             PagedList<TDanhMucSp> lst = new PagedList<TDanhMucSp>(lstsanpham, pageNumber, pageSize);
 
             return View(lst);
+        }
+        [Route("ThemSanPhamMoi")]
+        [HttpGet]
+        public IActionResult ThemSanPhamMoi()
+        {
+            ViewBag.MaChatLieu = new SelectList(db.TChatLieus.ToList(), "MaChatLieu", "ChatLieu");
+            ViewBag.MaHangSx = new SelectList(db.THangSxes.ToList(), "MaHangSX", "HangSX");
+            ViewBag.MaNuocSx = new SelectList(db.TQuocGia.ToList(), "MaNuoc", "TenNuoc");
+            ViewBag.MaLoai = new SelectList(db.TLoaiSps.ToList(), "MaLoai", "Loai");
+            ViewBag.MaDt = new SelectList(db.TLoaiDts.ToList(), "MaDt", "TenLoai");
+            return View();
+        }
+        [Route("ThemSanPhamMoi")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ThemSanPhamMoi(TDanhMucSp sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TDanhMucSps.Add(sanPham);
+                db.SaveChanges();
+                return RedirectToAction("DanhMucSanPham");
+            }
+            return View(sanPham);
         }
     }
 }
